527da20e0d542ff600afedd6fb5ce2c9cf38f073
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-02-07 22:13:02 +0400

    last commit

--- script.js
+++ script.js
@@ -1,129 +1,25 @@
-
-// document.addEventListener("DOMContentLoaded",function(){
-//     document.getElementById("punchId").addEventListener("click",function(){
-//         window.location.href="punch.html"
-//     })
-// })
-
-// document.addEventListener("DOMContentLoaded",function(){
-//     document.getElementById("login").addEventListener("click", function(){
-//         window.location.href="login.html"
-//     })
-// })
-
-// document.addEventListener("DOMContentLoaded",function(){
-//     document.getElementById("signUp").addEventListener("click",function(){
-//         window.location.href="register.html"
-//     })
-// })
-
-
-
-
-// !!!!!!!!-----------------------------------------
-
-// let myRegisterForm=document.querySelector(".myForm")
-// myRegisterForm.addEventListener("submit",function(event){
-//     event.preventDefault();
-//     let username=document.getElementById("fullname").value.trim();
-//     let password=document.getElementById("password").value.trim();
-//     let email=document.getElementById("email").value.trim();
-//     let phone=document.getElementById("phone").value.trim();
-
-
-//     const usernameRegex =/^[a-zA-Z0-9]{3,13}$/;
-//     const passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-//     const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-//     const phoneRegex=/^\d{6,18}$/;
     
-//     let isValid=true
-//     if(!usernameRegex.test(username)){
-//         document.getElementById("fullnameError").innerText=
-//         "Username must contain at least 3 characters, uppercase letter, and one digit."
-//         isValid=false
-//     }
-//     else{
-//         document.getElementById("fullnameError").innerText=""
-        
-//     }
-//     if(!phoneRegex.test(phone)){
-//         document.getElementById("phoneError").innerText=
-//         "Phone must be at least 6 characters long"
-//         isValid=false
-//     }
-//     else{
-//         document.getElementById("phoneError").innerText=""
-       
-//     }
-    
-//     if(!emailRegex.test(email)){
-//         document.getElementById("emailError").innerText=
-//         "Email must be a valid Gmail address (e.g., example@gmail.com)"
-//         isValid=false
-//     }
-//     else{
-//         document.getElementById("emailError").innerText=""
-       
-//     }
-    
-//     if(!passwordRegex.test(password)){
-//         document.getElementById("passwordError").innerText=
-//         "Password must be at least 6 characters long"
-//         isValid=false
-//     }
-//     else{
-//         document.getElementById("passwordError").innerText=""
-       
-//     }
-//     if(!isValid){
-//         return;
-//     }
-    
-//        let users = JSON.parse(localStorage.getItem("users")) || [];
-//        let existingUser = users.some(user => user.email === email);
-//        if(existingUser){
-//           alert("Bu email artiq mövcuddur!");
-//           return
-    
-//        }
-//        else{
-//           let newUser = {username,password,email,phone };
-//           users.push(newUser);
-//           localStorage.setItem("users", JSON.stringify(users));       
-//        }
-//        if(isValid){
-//         alert("qeydiyyatiniz ugurla elave olundu")
-//         myRegisterForm.reset();
-//        }
-    
-//     })
-
-    // document.querySelector(".myLogin").addEventListener("submit",function(event){
-    //     event.preventDefault();
-    
-    //     let loginEmail = document.getElementById("loginEmail").value.trim();
-    //     let loginPassword = document.getElementById("loginPassword").value.trim();
-    
-    //     let users=JSON.parse(localStorage.getItem("users"))||[];
-    //     let foundUser = users.find(user => user.email === loginEmail && user.password === loginPassword);
-    //     if(foundUser){
-    //         alert("Giris ugurla tamamlandi");
-    //         document.querySelector(".myLogin").reset()
-    //         document.getElementById("loginError").style.display="none";
-    //     }
-    //     else{
-    //          document.getElementById("loginError").innerText=
-    //        "Invalid password or Email"
-    //         document.getElementById("loginError").style.display=
-    //        "block"
-    //     }
-    // })
-      
 let myRegisterForm=document.querySelector(".myForm")
-let myLoginForm=document.querySelector(".myLogin")
+if(document.title.includes("Register")){
+  
+passEyeInput=document.getElementById("password")
+select=document.getElementById("select")
 
-if(myRegisterForm){
-   
+select.addEventListener("click",function(){
+    if(passEyeInput.type==="password"){
+        passEyeInput.type="text"
+       select.innerHTML=`
+        <i class="fa-solid fa-eye"></i>  `
+    }
+    else{
+        passEyeInput.type="password"
+        select.innerHTML=`
+      <i class="fa-solid fa-eye-slash"></i>
+        `
+    }
+})
+    
+
 myRegisterForm.addEventListener("submit",function(event){
     event.preventDefault();
     let username=document.getElementById("fullname").value.trim();
@@ -140,7 +36,7 @@ myRegisterForm.addEventListener("submit",function(event){
     let isValid=true
     if(!usernameRegex.test(username)){
         document.getElementById("fullnameError").innerText=
-        "Username must contain at least 3 characters, uppercase letter, and one digit."
+        "Username must contain at least 3 characters, uppercase letter."
         isValid=false
     }
     else{
@@ -200,7 +96,23 @@ myRegisterForm.addEventListener("submit",function(event){
     })
 }
 
-if(myLoginForm){
+
+
+
+if(document.title.includes("Login")){
+
+    document.getElementById("select2").addEventListener("click", function(){
+        if(document.getElementById("loginPassword").type==="password"){
+            document.getElementById("loginPassword").type="text"
+            document.getElementById("select2").innerHTML='<i class="fa-solid fa-eye"></i>'
+        }
+        else{
+              document.getElementById("loginPassword").type="password"
+              document.getElementById("select2").innerHTML=`<i class="fa-solid fa-eye-slash"></i>`
+        }
+    })
+
+    let myLoginForm=document.querySelector(".myLogin")
     myLoginForm.addEventListener("submit",function(event){
         event.preventDefault();
     
@@ -222,4 +134,6 @@ if(myLoginForm){
         }
     })
 }
+
+
 
